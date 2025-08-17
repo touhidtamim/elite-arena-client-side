38e344d7fa274989610cfd0f5c3bc53a10b484b2
Touhid Tamim <touhidtamim1@gmail.com>
2025-08-17 16:16:38 +0600

    updated spinner

--- src/Components/Shared/Spinner.jsx
+++ src/Components/Shared/Spinner.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { ScaleLoader } from "react-spinners";
 
-const Spinner = () => {
+const Spinner = ({ message = "Loading, please wait..." }) => {
   return (
-    <div className="flex justify-center items-center h-screen w-full">
+    <div className="flex flex-col justify-center items-center h-screen w-full">
       <ScaleLoader size={80} color="#ffffff" />
+      <p className="mt-6 text-white text-lg font-semibold animate-pulse text-center">
+        {message}
+      </p>
     </div>
   );
 };
